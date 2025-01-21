f6b1ad52512faa841309617bdcdceee0a8af4b36
menna essam <menna00essam@gmail.com>
2025-01-21 10:52:21 +0200

    edit

--- SignUp/signUp.js
+++ SignUp/signUp.js
@@ -18,36 +18,50 @@ function validFirstName() {
     const regExpressionName = /^[a-zA-Z]+$/;
     if (!regExpressionName.test(firstName.value)) {
         errorName.style.display = "block";
+        return 0;
     } else {
         errorName.style.display = "none";
+        return 1;
+
     }    
 }
 function validLasttName() {
     const regExpressionName = /^[a-zA-Z]+$/;
     if (!regExpressionName.test(lastName.value)) {
         errorName.style.display = "block";
+        return 0;
     } else {
         errorName.style.display = "none";
+        return 1;
+
     }    
 }  
 
 function vaildEmail() {
     const regExpressionEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!regExpressionEmail.test(email.value)) {
-        errorEmail.style.display  ="block";}
+        errorEmail.style.display  ="block";
+        return 0;
+    }
     else
     {
         errorEmail.style.display  ="none";
+        return 1;
+
 
     }       
 
 }
 function vaildPassword() {
     if (password.value.length < 8) {
-        errorPassword.style.display="block"  
+        errorPassword.style.display="block" ;
+        return 0;
+ 
     }
     {
-        errorPassword.style.display="none"  
+        errorPassword.style.display="none"  ;
+        return 1;
+
 
     }
 
@@ -56,19 +70,34 @@ function vaildPassword() {
 function validConfirmPassword() {
     if (password.value !== confirmPassword.value) {
       errorConfirmPassword.style.display = "block";
+      return 0;
+
     } else {
       errorConfirmPassword.style.display = "none";
+      return 1;
+
     }
   }
   function validCheckbox() {
     if (!agreePolicy.checked) {
       errorCheckbox.style.display = "block";
+      return 0;
+
      
     } else {
       errorCheckbox.style.display = "none";
+      return 1;
+
       
     }
   }
+  firstName.addEventListener("input", validFirstName);
+    lastName.addEventListener("input", validLasttName);
+    email.addEventListener("input", vaildEmail);
+    password.addEventListener("input", vaildPassword);
+    confirmPassword.addEventListener("input", validConfirmPassword);
+    agreePolicy.addEventListener("change", validCheckbox);
+  
   function FormSubmit(e) {
     e.preventDefault();
 
@@ -87,7 +116,10 @@ function validConfirmPassword() {
       localStorage.setItem("password", password.value); 
       
       SucessMessage.style.display = "block";
-      window.location.href = "../Login/login.html";
+      setTimeout(() => {
+        window.location.href = "../Login/login.html";
+
+      }, 1000);
 
   }
 }
@@ -117,12 +149,6 @@ submitBottun.addEventListener("click" , FormSubmit)
   
     loadFormData();
   
-    firstName.addEventListener("input", validFirstName);
-    lastName.addEventListener("input", validLasttName);
-    email.addEventListener("input", vaildEmail);
-    password.addEventListener("input", vaildPassword);
-    confirmPassword.addEventListener("input", validConfirmPassword);
-    agreePolicy.addEventListener("change", validCheckbox);
-  
+    
   
   
