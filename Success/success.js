cbc4ea01430ddcd1f06238d235a303e95151f139
menna essam <menna00essam@gmail.com>
2025-01-25 12:55:28 +0200

    add page sucess

--- Success/success.js
+++ Success/success.js
@@ -3,3 +3,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const Score = urlParams.get("score");
 let resultPara = document.querySelector(".result");
 resultPara.innerHTML = Score;
+
+
+const username = localStorage.getItem("username");
+  document.getElementById("username").textContent = username;
